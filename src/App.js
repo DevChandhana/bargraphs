4e76fe54a3286f6581e980b88a5d01502ea1db25
Chandhana <chandhanareddy1997@gmail.com>
2022-03-29 18:16:42 +0530

    bar graphs with d3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import * as d3 from "d3";
+import { data } from "./utils";
+import "./App.css";
+const width = 950;
+const height = 1000;
+const margin = { top: 200, right: 20, bottom: 20, left: 20 };
 
-function App() {
+const App = () => {
+  const innerheight = height - margin.top - margin.bottom;
+  const innerwidth = width - margin.left - margin.right;
+
+  const yScale = d3
+    .scaleBand()
+    .domain(data.map((d) => d.date))
+    .range([0, innerheight])
+    .padding(0.4)
+    .paddingOuter(0.4);
+
+  const xScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d.value)])
+    .range([0, innerwidth]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <svg width={width} height={height}>
+        <g transform={`translate(${margin.top}, ${margin.left})`}>
+          {/* ticks */}
+
+          {xScale.ticks().map((tickValue) => (
+            <g transform={`translate(${xScale(tickValue)}, 0)`}>
+              <line y2={innerheight} stroke="#C0C0BB" />
+              <text
+                y={innerheight + 3}
+                dy=".71em"
+                style={{ textAnchor: "middle", zIndex: 100 }}
+              >
+                {tickValue}
+              </text>
+            </g>
+          ))}
+          {yScale.domain().map((tickValue) => (
+            <g
+              transform={`translate(0,${
+                yScale(tickValue) + yScale.bandwidth() / 2
+              })`}
+            >
+              <text style={{ textAnchor: "end", fontSize: 8 }} dy=".32em">
+                {tickValue}
+              </text>
+            </g>
+          ))}
+          {data.map((item) => (
+            <rect
+              x={0}
+              y={yScale(item.date)}
+              width={xScale(item.value)}
+              height={yScale.bandwidth()}
+              className="mark"
+            />
+          ))}
+        </g>
+      </svg>
     </div>
   );
-}
-
+};
 export default App;
